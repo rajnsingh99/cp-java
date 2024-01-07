49fd0b0aa55287f4b07830a8a05bedd31df01c6f
Raj Singh <rajnsingh99@gmail.com>
2024-01-07 23:09:50 +0530

    Build-2

--- src/Difficulty800/ProblemSet800.java
+++ src/Difficulty800/ProblemSet800.java
@@ -1,8 +1,32 @@
 package Difficulty800;
 
 public class ProblemSet800 {
-
     public static void main(String [] args) {
-        System.out.println("CodeForce ProblemSet 800");
+        System.out.println("CodeForce ProblemSet 800 \n");
+
+
     }
+
+    private static void watermelon(int weight) {
+        System.out.println("Weight ---> " + weight);
+        if (weight % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    private static void wayToLongWords(String word) {
+        // StringBuilder is not thread safe and faster than StringBuffer
+        if (word.length() > 10) {
+            String abbreviatedWord = String.valueOf(word.charAt(0)) +
+                    (word.length() - 2) +
+                    word.charAt(word.length() - 1);
+            System.out.println(abbreviatedWord);
+        } else {
+            System.out.println(word);
+        }
+    }
+
+
 }
