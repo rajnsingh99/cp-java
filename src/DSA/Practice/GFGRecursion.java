625b7c5b7406e1d9a233b879a55e71088c317225
Raj Narayan Singh <rajnsingh99@gmail.com>
2024-12-02 10:29:50 +0530

    Build-9

--- src/DSA/Practice/GFGRecursion.java
+++ src/DSA/Practice/GFGRecursion.java
@@ -1,11 +1,21 @@
 package DSA.Practice;
 
+import java.util.*;
 
 public class GFGRecursion {
 
-    public static void main(String args[]) {
-        int[] coins = {1,2,3};
-        System.out.println(count(coins, coins.length, 5));
+    public static void main(String[] args) {
+//       Stack<Integer> stack = new Stack<>();
+//       stack.add(-3);
+//       stack.add(14);
+//       stack.add(18);
+//       stack.add(-5);
+//       stack.add(30);
+//       System.out.println(stack);
+//       sortStack(stack);
+//       System.out.println(stack);
+
+        printAllPalindromicPartitions("nitin");
     }
 
     private static void print1ToN(int n) {
@@ -113,6 +123,140 @@ public class GFGRecursion {
         // including coins[n-1] (ii) excluding coins[n-1]
         return count(coins, n - 1, sum)
                 + count(coins, n, sum - coins[n - 1]);
+    }
+
+    private static void floodFill(int[][] arr, int x, int y) {
+        if(arr[x][y] == 1) {
+            arr[x][y] = 3;
+        }
+
+        if (x < 2)
+            floodFill(arr, x + 1, y);
+        if (y < 2)
+            floodFill(arr, x, y + 1);
+    }
+
+    private static Integer reverseQueue(Queue<Integer> queue, int size) {
+        if(size == 0) {
+            return queue.remove();
+        }
+
+
+        Integer num = reverseQueue(queue, size - 1);
+        queue.add(num);
+        return num;
+    }
+
+    private static void pyramid(int n, int size) {
+        if(n == size) {
+            return;
+        }
+
+        for (int i = 0; i<= n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        pyramid(n + 1, size);
+    }
+
+    private static String longestPalindrome(String str, int n) {
+        if(str.length() - 1 == n) {
+            return String.valueOf(str.charAt(n));
+        }
+
+        String subStr = str.charAt(n) + longestPalindrome(str, n + 1);
+        System.out.println("----> " + subStr + " --> " + checkPalindrome(subStr));
+        return subStr;
+    }
+
+    private static boolean checkPalindrome(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            stringBuilder.append(str.charAt(i));
+        }
+
+        return str.equalsIgnoreCase(stringBuilder.toString());
+    }
+
+    private static void subset(String input, String output) {
+        if(input.isEmpty()) {
+            System.out.println("output > " + output);
+            return;
+        }
+
+        String op1 = output;
+        String op2 =  output + input.charAt(0);
+        String new_input = input.substring(1);
+        subset(new_input, op1);
+        subset(new_input, op2);
+    }
+
+    private static void subset2(int[] output, int[] input) {
+        if(input.length == 0) {
+            System.out.println(Arrays.toString(output));
+            return;
+        }
+
+        List<Integer> output2 = new ArrayList<>(Arrays.stream(output).boxed().toList());
+        output2.add(input[0]);
+        List<Integer> newInput = new ArrayList<>(Arrays.stream(input).boxed().toList());
+        newInput.removeFirst();
+        subset2(output, newInput.stream().mapToInt(Integer::intValue).toArray());
+        subset2(output2.stream().mapToInt(Integer::intValue).toArray(), newInput.stream().mapToInt(Integer::intValue).toArray());
+    }
+
+    private static void sortStack(Stack<Integer> stack) {
+        if(stack.size() == 1) {
+            return;
+        }
+
+        int prevValue = stack.pop();
+        sortStack(stack);
+        System.out.println("-> " + stack);
+
+        if(prevValue > stack.peek()) {
+           insertItemStack(stack, prevValue);
+        } else {
+            stack.add(prevValue);
+        }
+    }
+
+    private static void insertItemStack(Stack<Integer> stack, int valueToInsert) {
+        if(stack.empty()) {
+            stack.add(valueToInsert);
+            return;
+        }
+        if(stack.peek() < valueToInsert) {
+            int prevValue = stack.pop();
+            insertItemStack(stack, valueToInsert);
+            stack.add(prevValue);
+        } else {
+            stack.add(valueToInsert);
+        }
+    }
+
+    private static void printAllPalindromicPartitions(String input) {
+        Set<String> subsets = new HashSet<>();
+        getAllCombinations(input, "", subsets);
+        for(String subset : subsets){
+            if (input.contains(subset)) {
+                System.out.println(subset);
+            }
+        }
+    }
+
+    private static void getAllCombinations(String input, String output, Set<String> subsets) {
+        if(input.isEmpty()) {
+            if(checkPalindrome(output)) {
+                subsets.add(output);
+            }
+            return;
+        }
+
+        String op2 =  output + input.charAt(0);
+        String new_input = input.substring(1);
+        getAllCombinations(new_input, output, subsets);
+        getAllCombinations(new_input, op2, subsets);
     }
 
     /**
